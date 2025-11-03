a256bc605382eab2ef0b85ed398482da824c2d7e
Shmudi <143405827+shmudi@users.noreply.github.com>
2025-11-03 16:05:40 +0100

    Drizzle: legg til dialect=sqlite i config; fikser generate-feil

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
-  out: './drizzle',
-  dialect: 'sqlite',
-  driver: 'd1-http',
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dialect: "sqlite", 
+  driver: "d1-http",
   dbCredentials: {
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+    accountId: "5af019aacbcddd891500ad12c0b6323d",
+    databaseId: "933cc601-b52d-4886-be7d-27e49d68eb6a",
     token: process.env.CLOUDFLARE_D1_TOKEN!,
   },
 });
